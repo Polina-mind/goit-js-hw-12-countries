47143b950e785820e4591bca0a435eb00fce3892
Polina-mind <popova.v.polina@gmail.com>
2021-02-10 16:00:07 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import './styles.css';
+const debounce = require('lodash.debounce');
+import fetchCountries from './js/fetchCountries';
+import './styles.scss';
+
+console.log(fetchCountries);
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,7 @@
+function fetchCountries(searchQuery) {
+  const promise = new Promise(() => {
+    console.log('Hello');
+  }, 1000);
+}
+
+export default fetchCountries;
