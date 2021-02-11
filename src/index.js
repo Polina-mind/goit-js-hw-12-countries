a80d8b9420eb00fd9ad2d04071b8b7ddc41c54a7
Polina-mind <popova.v.polina@gmail.com>
2021-02-11 23:54:03 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-const debounce = require('lodash.debounce');
+var debounce = require('lodash.debounce');
 import fetchCountries from './js/fetchCountries';
 import './styles.scss';
 
-console.log(fetchCountries);
+const refs = {
+  nameInput: document.querySelector('#country-input'),
+  countryOption: document.querySelector('.country-option'),
+};
+
+refs.nameInput.addEventListener(
+  'input',
+  debounce(event => {
+    const inputString = event.target.value;
+    if (inputString.length > 0) {
+      fetchCountries(inputString);
+    } else {
+      refs.countryOption.innerHTML = '';
+    }
+  }, 500),
+);
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,38 @@
+import finalTemplate from '../templates/final-template.hbs';
+import template from '../templates/template.hbs';
+
+const refs = {
+  nameInput: document.querySelector('#country-input'),
+  countryOption: document.querySelector('.country-option'),
+};
+
+const url =
+  'https://restcountries.eu/rest/v2/all?fields=name;capital;population;languages;flag';
+
 function fetchCountries(searchQuery) {
-  const promise = new Promise(() => {
-    console.log('Hello');
-  }, 1000);
+  fetch(url)
+    .then(response => response.json())
+    .then(countryData => {
+      // console.log(countryData);
+      const filteredCountry = countryData.filter(country =>
+        country.name.toLowerCase().includes(searchQuery.toLowerCase()),
+      );
+      //   console.log(filteredCountry);
+      if (filteredCountry.length === 1) {
+        const markup = finalTemplate(filteredCountry);
+        refs.countryOption.innerHTML = markup;
+      } else if ((filteredCountry.length > 1) & (filteredCountry.length < 10)) {
+        const markup = template(filteredCountry);
+        refs.countryOption.innerHTML = markup;
+      } else if (filteredCountry.length >= 10) {
+        refs.countryOption.innerHTML =
+          '<p class="helper">Введите более конкретное название</p>';
+      } else {
+        refs.countryOption.innerHTML =
+          '<p class="helper">Введение некорректного поискового запроса</p>';
+      }
+    })
+    .catch(error => console.log(error));
 }
 
 export default fetchCountries;
